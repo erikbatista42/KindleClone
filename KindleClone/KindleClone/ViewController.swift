bb3286c03b816b8ea681d5607969b22c2f4cb608
Erik <erikbatista42@gmail.com>
2017-07-03 07:01:16 -0400

    Used for loops to print out pages

--- KindleClone/KindleClone/ViewController.swift
+++ KindleClone/KindleClone/ViewController.swift
@@ -38,18 +38,35 @@ class ViewController: UIViewController {
         //Can provide custom code starting here
         
         let page1 = Page(number: 1, text: "Text for the first page")
-        let page2 = Page(number: 2, text: "this is text for second page")
+        let page2 = Page(number: 2, text: "This is text for second page")
         
-        print(page1.text)
-        print(page2.text)
-        
+
         let pages = [page1,page2]
         
         let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
-        print(book.title,book.author)
         
-        let firstPage = book.pages[0]
-        print(firstPage.text)
+//        let firstPage = book.pages[0]
+//        
+//        if page2.number < 3 {
+//            print(page2.text)
+//        }
+//        book.pages[0]
+//        book.pages[1]
+        
+        
+        let book2 =  Book(title: "Bill Gates: A Biography", author: "Michael Becraft", pages: [
+            Page(number: 1, text: "Text for page 1"),
+            Page(number: 2, text: "Text for page 2"),
+            Page(number: 3, text: "Text for page 3"),
+            Page(number: 4, text: "Text for page 4")
+            ])
+        for books in [book, book2] {
+            print(books.title)
+            for pages in books.pages {
+                print(pages.text)
+            }
+        }
+        
     }
 
 }
