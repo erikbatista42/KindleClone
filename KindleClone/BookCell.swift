c578c62cf30b34308c0dde4d5328955cd16c6a41
Erik <erikbatista42@gmail.com>
2017-07-03 18:00:39 -0400

    adding subviews using frames

--- KindleClone/BookCell.swift
+++ KindleClone/BookCell.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class BookCell: UITableViewCell {
+    let coverImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This is the text for the title of our book inside of out cell"
+        label.backgroundColor = .blue
+        return label
+    }()
+    
+    let authorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Thisi some author for the boook that we have in this row"
+         label.backgroundColor = .green
+        return label
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         print("Cell is being initialized..")
         backgroundColor = .yellow
+        
+        addSubview(coverImageView)
+        coverImageView.frame = CGRect(x: 8, y: 8, width: 50, height: 64)
+        
+        addSubview(titleLabel)
+        titleLabel.frame = CGRect(x: 66, y: 20, width: 200, height: 20)
+        
+        addSubview(authorLabel)
+        authorLabel.frame = CGRect(x: 66, y: 48, width: 200, height: 20)
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coderL) has not been implemented")
